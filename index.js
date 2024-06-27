3e5753c7a70aebc5ede94db8e8b4644c759379b5
Khaerul Fikri <khaerulfikri99@gmail.com>
2024-06-28 00:44:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ const dbConfig = {
   host: '153.92.15.3',
   user: 'u357102271_OTP',
   password: 'FikriLuksi321@@@',
-  database: 'u357102271_OTP'
+  database: 'u357102271_OTP',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 };
+
+const pool = mysql.createPool(dbConfig);
 
 const stored_api_key = 'FikriLuksi321@@@'
 
@@ -36,9 +41,11 @@ app.get('/webhook', async (req, res) => {
   `;
   
   try {
-      const connection = await mysql.createConnection(dbConfig);
+      const connection = await pool.getConnection();
       const [results] = await connection.execute(query);
-      const affected = 0
+      connection.release();  // Ensure connection is released back to the pool
+      
+      let affected = 0;
       await Promise.all(results.map(async (row) => {
           try {
               const url = `${row.url_getmessage}?apikey=${row.api_key}&id=${row.order_id}`;
@@ -47,13 +54,13 @@ app.get('/webhook', async (req, res) => {
                   const responseData = response.data;
                   if(responseData.status == "success") {
                     if (responseData.data.status == 3) {
-                      affected = await updateOrder('Done', 'Success', responseData.data.inbox, 'Unread', row.order_id);
+                      affected += await updateOrder('Done', 'Success', responseData.data.inbox, 'Unread', row.order_id);
                       const url2 = `${row.url_cancel}?apikey=${row.api_key}&id=${row.order_id}&status=1`;
                       await axios.get(url2, { timeout: 5000 });
                     } else if (responseData.data.status == 0) {
-                      affected = await updateOrder('Canceled', 'Timeout', '', 'Read', row.order_id);
+                      affected += await updateOrder('Canceled', 'Timeout', '', 'Read', row.order_id);
                     } else if (responseData.data.status == 1) {
-                      affected = await updateOrder('Done', 'Success', responseData.data.inbox, 'Unread', row.order_id);
+                      affected += await updateOrder('Done', 'Success', responseData.data.inbox, 'Unread', row.order_id);
                     }
                   }                  
               } else {
@@ -63,7 +70,6 @@ app.get('/webhook', async (req, res) => {
               console.error(`Error making GET request Webhook:`, error.message);
           }
       }));
-    await connection.end();
     return res.status(200).json({
       "status":"success",
       "data":affected,
@@ -80,7 +86,7 @@ app.get('/webhook', async (req, res) => {
 });
 
 app.get('/cancel-order', async(req, res) => {
-  const order_id = req.query.order_id
+  const order_id = req.query.order_id;
   const query = `
       SELECT A.id as order_id,A.*, B.cost, C.*
       FROM tbl_order AS A
@@ -90,9 +96,9 @@ app.get('/cancel-order', async(req, res) => {
   `;
   
   try {
-      const connection = await mysql.createConnection(dbConfig);
+      const connection = await pool.getConnection();
       const [results] = await connection.execute(query);
-      await connection.end();
+      connection.release();
 
       if(results.length == 0) {
         return res.status(200).json({
@@ -101,9 +107,9 @@ app.get('/cancel-order', async(req, res) => {
         });
       }
 
-      const row = results[0]
+      const row = results[0];
 
-      console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}'`)
+      console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}'`);
 
       const url = `${row.url_getmessage}?apikey=${row.api_key}&id=${row.order_id}`;
         const response = await axios.get(url, { timeout: 5000 });
@@ -111,7 +117,7 @@ app.get('/cancel-order', async(req, res) => {
             const responseData = response.data;
             if(responseData.status == "success") {
               if (responseData.data.status == 3) {
-                console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}' Success`)
+                console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}' Success`);
                 return res.status(200).json({
                   "status":"success",
                   "message":"Order tidak dibatalkan, OTP sudah diterima"
@@ -119,7 +125,7 @@ app.get('/cancel-order', async(req, res) => {
               } else {
                 const url2 = `${row.url_cancel}?apikey=${row.api_key}&id=${row.order_id}&status=0`;
                 await axios.get(url2, { timeout: 5000 });
-                console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}' Success`)
+                console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}' Success`);
                 return res.status(200).json({
                   "status":"success",
                   "message":"Order berhasil dibatalkan"
@@ -129,7 +135,7 @@ app.get('/cancel-order', async(req, res) => {
         } else {
           const url2 = `${row.url_cancel}?apikey=${row.api_key}&id=${row.order_id}&status=0`;
           await axios.get(url2, { timeout: 5000 });
-          console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}' Success`)
+          console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}' Success`);
           return res.status(200).json({
             "status":"success",
             "message":"Order berhasil dibatalkan"
@@ -137,89 +143,97 @@ app.get('/cancel-order', async(req, res) => {
         }
   } catch (error) {
       console.error('Error executing query:', error);
-      console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}' Failed`)
+      console.log(`'${row.username}' canceling '${row.application}' with number '${row.number}' Failed`);
       return res.status(200).json({
         "status":"error",
         "message":"Order gagal, silahkan coba beberapa saat lagi."
       });
   }
-})
+});
 
 app.post('/create-order', async (req, res) => {
   const { application_id, username } = req.body;
 
-  const connection = await mysql.createConnection(dbConfig);
-  const [resQ] = await connection.execute(`SELECT A.*,A.id as app_id,B.*,C.quota FROM tbl_application AS A JOIN tbl_config AS B JOIN tbl_customer AS C WHERE A.id='${application_id}'`);
-  await connection.end();
-  if (resQ.length == 0) {
-    return res.status(200).json({
-      "status":"error",
-      "message":"Application not found"
-    });
-  }
-
-  const row = resQ[0]
-
-  if (row.quota < row.cost) {
-    return res.status(200).json({
-      "status":"error",
-      "message":"Order gagal, silahkan coba beberapa saat lagi."
-    });
-  }
-
-  const currentDateTime = new Date(new Date().getTime() + (7 * 60 * 60 * 1000)).toISOString().replace('T', ' ').substring(0, 19);
-
-  //SendAPI timeout 5 detik
   try {
-    const response = await axios.get(`${row.url_order}?apikey=${row.api_key}&service=${application_id}&operator=${row.operator}&country=${row.country_id}`, {timeout: 10000});
-    if (response.status === 200) {
-        const responseData = response.data;
-        if(responseData.status == "success") {
-          try {
-            console.log(`'${username}' Order '${row.application}' with number '${responseData.number}'`)
-            const query_insert = `INSERT INTO tbl_order (id, username, application, order_time, number, status, order_status, read_status) 
-                          VALUES ('${responseData.id}','${username}', '${row.application}', '${currentDateTime}', '${responseData.number}', 'Waiting Sms', 'Ongoing', 'New')`;
-            const connection = await mysql.createConnection(dbConfig);
-            const insert_status = await connection.execute(query_insert);
-            await connection.end();  
-            if(insert_status) {            
-              return res.status(200).json(responseData)
-            } else {
-              return res.status(200).json({
-                "status":"error",
-                "message":"Order gagal, silahkan coba beberapa saat lagi."
-              });
+    const connection = await pool.getConnection();
+    const [resQ] = await connection.execute(`SELECT A.*,A.id as app_id,B.*,C.quota FROM tbl_application AS A JOIN tbl_config AS B JOIN tbl_customer AS C WHERE A.id='${application_id}'`);
+    connection.release();
+    if (resQ.length == 0) {
+      return res.status(200).json({
+        "status":"error",
+        "message":"Application not found"
+      });
+    }
+
+    const row = resQ[0];
+
+    if (row.quota < row.cost) {
+      return res.status(200).json({
+        "status":"error",
+        "message":"Order gagal, silahkan coba beberapa saat lagi."
+      });
+    }
+
+    const currentDateTime = new Date(new Date().getTime() + (7 * 60 * 60 * 1000)).toISOString().replace('T', ' ').substring(0, 19);
+
+    //SendAPI timeout 5 detik
+    try {
+      const response = await axios.get(`${row.url_order}?apikey=${row.api_key}&service=${application_id}&operator=${row.operator}&country=${row.country_id}`, {timeout: 10000});
+      if (response.status === 200) {
+          const responseData = response.data;
+          if(responseData.status == "success") {
+            try {
+              console.log(`'${username}' Order '${row.application}' with number '${responseData.number}'`);
+              const query_insert = `INSERT INTO tbl_order (id, username, application, order_time, number, status, order_status, read_status) 
+                            VALUES ('${responseData.id}','${username}', '${row.application}', '${currentDateTime}', '${responseData.number}', 'Waiting Sms', 'Ongoing', 'New')`;
+              const connection = await pool.getConnection();
+              const [insert_status] = await connection.execute(query_insert);
+              connection.release();
+              if(insert_status) {            
+                return res.status(200).json(responseData);
+              } else {
+                return res.status(200).json({
+                  "status":"error",
+                  "message":"Order gagal, silahkan coba beberapa saat lagi."
+                });
+              }
+            } catch (error) {
+              console.log(`'${username}' Order Failed`);
+                console.error('Error executing query:', error);
+                return res.status(200).json({
+                  "status":"error",
+                  "message":"Order gagal, silahkan coba beberapa saat lagi."
+                });
             }
-          } catch (error) {
-            console.log(`'${username}' Order Failed`)
-              console.error('Error executing query:', error);
+          } else {
+            if(responseData.message == "saldo tidak mencukupi") {
+              console.log(`'${username}' Order Failed, out of balance`);
               return res.status(200).json({
                 "status":"error",
-                "message":"Order gagal, silahkan coba beberapa saat lagi."
+                "message":"Order gagal, Stock kartu habis, silahkan hubungi CS terkait kendala ini."
               });
-          }
-        } else {
-          if(responseData.message == "saldo tidak mencukupi") {
-            console.log(`'${username}' Order Failed, out of balance`)
-            return res.status(200).json({
-              "status":"error",
-              "message":"Order gagal, Stock kartu habis, silahkan hubungi CS terkait kendala ini."
-            });
-          } else {
-            return res.status(200).json(responseData)
-          }
-        }                 
-    } else {
-      console.log(`'${username}' Order Failed, Respond server != 200`)
-        console.error(`Failed GET request Webhook status code: ${response.status}`);
-        return res.status(200).json({
-          "status":"error",
-          "message":"Order gagal, silahkan coba beberapa saat lagi."
-        });
-    } 
+            } else {
+              return res.status(200).json(responseData);
+            }
+          }                 
+      } else {
+        console.log(`'${username}' Order Failed, Respond server != 200`);
+          console.error(`Failed GET request Webhook status code: ${response.status}`);
+          return res.status(200).json({
+            "status":"error",
+            "message":"Order gagal, silahkan coba beberapa saat lagi."
+          });
+      } 
+    } catch (error) {
+      console.log(`'${username}' Order Failed, Error Send GET ${error}`);
+      console.error('Error Send GET:', error);
+      return res.status(200).json({
+        "status":"error",
+        "message":"Order gagal, silahkan coba beberapa saat lagi."
+      });
+    }
   } catch (error) {
-    console.log(`'${username}' Order Failed, Error Send GET ${error}`)
-    console.error('Error Send GET:', error);
+    console.error('Error getting connection:', error);
     return res.status(200).json({
       "status":"error",
       "message":"Order gagal, silahkan coba beberapa saat lagi."
@@ -230,9 +244,9 @@ app.post('/create-order', async (req, res) => {
 const updateOrder = async (order_status, status, message, read_status, id) => {
   const query = `UPDATE tbl_order SET order_status = ?, status = ?, message = ?, read_status = ? WHERE id = ?`;
   try {
-      const connection = await mysql.createConnection(dbConfig);
+      const connection = await pool.getConnection();
       const [results] = await connection.execute(query, [order_status, status, message, read_status, id]);
-      await connection.end();
+      connection.release();
       return results.affectedRows;
   } catch (err) {
       console.error('Error updating order:', err);
@@ -247,9 +261,9 @@ app.get('/show-orders', async (req, res) => {
   const query = `SELECT A.*, B.cost, C.email FROM tbl_order AS A LEFT JOIN tbl_application AS B ON B.application = A.application LEFT JOIN tbl_customer AS C ON C.username = A.username WHERE ${whereClause}`;
   
   try {
-    const connection = await mysql.createConnection(dbConfig);
+    const connection = await pool.getConnection();
     const [results] = await connection.execute(query);
-    await connection.end();
+    connection.release();
     res.json(results);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -263,9 +277,9 @@ app.get('/customers-config', async (req, res) => {
   const query = `SELECT A.*, B.* FROM tbl_customer AS A JOIN tbl_config AS B WHERE ${whereClause}`;
   
   try {
-    const connection = await mysql.createConnection(dbConfig);
+    const connection = await pool.getConnection();
     const [results] = await connection.execute(query);
-    await connection.end();
+    connection.release();
     res.json(results);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -277,9 +291,9 @@ app.get('/applications', async (req, res) => {
   const query = 'SELECT * FROM tbl_application';
   
   try {
-    const connection = await mysql.createConnection(dbConfig);
+    const connection = await pool.getConnection();
     const [results] = await connection.execute(query);
-    await connection.end();
+    connection.release();
     res.json(results);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -292,9 +306,9 @@ app.put('/update-markread', async (req, res) => {
   const query = `UPDATE tbl_order SET read_status = 'Read' WHERE id = '${id}' AND order_status = 'Done'`;
   
   try {
-    const connection = await mysql.createConnection(dbConfig);
+    const connection = await pool.getConnection();
     const [results] = await connection.execute(query);
-    await connection.end();
+    connection.release();
     return res.status(200).json({
       "status":"success",
       "message":"Mark as read success"
@@ -314,9 +328,9 @@ app.post('/insert-customer', async (req, res) => {
   const values = [username, password, quota, email];
   
   try {
-    const connection = await mysql.createConnection(dbConfig);
+    const connection = await pool.getConnection();
     const [results] = await connection.execute(query, values);
-    await connection.end();
+    connection.release();
     res.json({ success: true, results });
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -327,4 +341,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
